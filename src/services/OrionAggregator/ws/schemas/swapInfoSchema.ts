28b466b15fcb49dafdde934d08e8d0e3a7f03469
Aleksandr Kraiz <selfsurfer@gmail.com>
2022-02-23 20:06:24 +0300

    Fix schema

--- src/services/OrionAggregator/ws/schemas/swapInfoSchema.ts
+++ src/services/OrionAggregator/ws/schemas/swapInfoSchema.ts
@@ -6,14 +6,14 @@ const swapInfoSchema = baseMessageSchema.extend({
   T: z.literal(MessageType.SWAP_INFO),
   S: z.string(), // swap request id
   ai: z.string(), // asset in,
-  a: z.string(), // amount in
   ao: z.string(), // asset out
-  o: z.string(), // amount out
-  p: z.string(), // price
-  mo: z.string(), // market amount out
-  mp: z.string(), // market price
-  ma: z.string(), // min amount
-  aa: z.string(), // available amount in
+  a: z.number(), // amount in
+  o: z.number(), // amount out
+  p: z.number(), // price
+  mo: z.number(), // market amount out
+  mp: z.number(), // market price
+  ma: z.number(), // min amount
+  aa: z.number(), // available amount in
   ps: z.string().array(), // path
   po: z.boolean(), // is swap through pool optimal
 });
