4c85dbe3b60fd55c3dea75e064a0a94098145707
Aleksandr Kraiz <selfsurfer@gmail.com>
2022-02-24 17:42:31 +0300

    Added VOB support

--- src/services/OrionAggregator/ws/schemas/orderBookSchema.ts
+++ src/services/OrionAggregator/ws/schemas/orderBookSchema.ts
@@ -6,6 +6,10 @@ export const orderBookItemSchema = z.tuple([
   z.string(), // price
   z.string(), // size
   z.array(z.string()), // exchanges
+  z.array(z.tuple([
+    z.enum(['SELL', 'BUY']), // side
+    z.string(), // pairname
+  ])),
 ]);
 
 export const orderBookSchema = baseMessageSchema.extend({
