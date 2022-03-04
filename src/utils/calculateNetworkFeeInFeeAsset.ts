13315cab5bb5a2515ec8c03821e8edaae87e9bef
Aleksandr Kraiz <selfsurfer@gmail.com>
2022-03-04 14:01:08 +0300

    Added fee calc notice

--- src/utils/calculateNetworkFeeInFeeAsset.ts
+++ src/utils/calculateNetworkFeeInFeeAsset.ts
@@ -8,12 +8,25 @@ const calculateNetworkFeeInFeeAsset = (
   feeAssetPriceInOrn: string,
 ) => {
   const networkFee = calculateNetworkFee(gasPrice, gasLimit);
+
   const networkFeeInOrn = new BigNumber(networkFee).multipliedBy(baseCurrencyPriceInOrn);
   const networkFeeInFeeAsset = networkFeeInOrn
     .multipliedBy(
       new BigNumber(1)
         .div(feeAssetPriceInOrn),
     );
+
+  console.log(`
+    Вычисляем комиссию сети в ассете комиссии.
+    Берем комиссию в нативной криптовалюте ${networkFee.toString()}
+    Умножаем на цену нативной криптовалюты в Орионах (${baseCurrencyPriceInOrn})
+    Умножаем полученное (комиссия сети в Орионах) на развернутую цену ассета комиссии в орионах ${new BigNumber(1).div(feeAssetPriceInOrn).toString()}
+    Итого: ${networkFee.toString()} 
+    * ${baseCurrencyPriceInOrn} 
+    * ${new BigNumber(1).div(feeAssetPriceInOrn).toString()}
+    = ${networkFeeInFeeAsset.toString()}
+  `);
+
   return networkFeeInFeeAsset.toString();
 };
 
