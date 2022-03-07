139af39cb6bb2cad979bea564a3b5ea799d07042
Aleksandr Kraiz <selfsurfer@gmail.com>
2022-03-07 11:05:33 +0300

    Added generateSecret

---
+++ src/utils/generateSecret.ts
@@ -0,0 +1,12 @@
+import rand from 'csprng';
+import { ethers } from 'ethers';
+
+const generateSecret = () => {
+  const RANDOM_RADIX = 16;
+  const RANDOM_BITS = 256;
+  const random = rand(RANDOM_BITS, RANDOM_RADIX);
+  const secret = ethers.utils.keccak256(random);
+  return secret;
+};
+
+export default generateSecret;
