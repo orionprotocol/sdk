8bca81cb378a6f10ce4589ff7dc7576091ff900e
Aleksandr Kraiz <selfsurfer@gmail.com>
2022-04-11 16:13:48 +0400

    Fix getSwapSide

--- src/utils/getSideByPair.ts
+++
@@ -1,7 +0,0 @@
-export default function getSideByPair(pair: string | undefined) {
-  if (!pair) return undefined;
-  const assets = pair.split('-');
-  if (assets.length !== 2) throw new Error('Invalid pair');
-  const [baseAsset] = assets;
-  return baseAsset === 'USDT' ? 'BUY' : 'SELL';
-}
