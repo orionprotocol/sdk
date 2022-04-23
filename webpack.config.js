f4297f2a0dc2ff7e641b77a6fff7b702bc34cc2e
Aleksandr Kraiz <selfsurfer@gmail.com>
2022-04-24 01:24:39 +0400

    Webpack / ESM

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      index: path.resolve(__dirname, "./lib/esm/index.js")
+    },
+    output: {
+      path: path.resolve(__dirname, "./lib/umd"), // builds to ./lib/umd/
+      filename: "[name].js", // index.js
+      library: "orionprotocol", // aka window.myLibrary
+      libraryTarget: "umd", // supports commonjs, amd and web browsers
+      globalObject: "this"
+    },
+    module: {
+      rules: [{ test: /\.t|js$/, use: "babel-loader" }]
+    },
+    resolve: {
+        fallback: {
+            "crypto": require.resolve("crypto-browserify"),
+            "buffer": require.resolve("buffer/"),
+            "stream": require.resolve("stream-browserify"),
+        }
+    }
+  };
+};
