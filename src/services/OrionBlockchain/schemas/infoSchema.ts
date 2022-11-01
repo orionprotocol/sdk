1f22be1527906f9f29fff35b5d87147711412717
Aleksandr Kraiz <selfsurfer@gmail.com>
2022-11-01 15:00:50 +0400

    Added internalFeeAssets

--- src/services/OrionBlockchain/schemas/infoSchema.ts
+++ src/services/OrionBlockchain/schemas/infoSchema.ts
@@ -1,6 +1,12 @@
 import { z } from 'zod';
 import { makePartial } from '../../../utils';
 
+const internalFeeAssetSchema = z.object({
+  type: z.enum(['percent', 'plain']),
+  value: z.number(),
+  asset: z.string(),
+});
+
 const infoSchema = z.object({
   chainId: z.number(),
   chainName: z.string(),
@@ -11,6 +17,8 @@ const infoSchema = z.object({
   assetToAddress: z.record(z.string()).transform(makePartial),
   assetToDecimals: z.record(z.number()).transform(makePartial),
   assetToIcons: z.record(z.string()).transform(makePartial).optional(),
+  cexTokens: z.string().array(),
+  internalFeeAssets: internalFeeAssetSchema.array().optional(),
 });
 
 export default infoSchema;
