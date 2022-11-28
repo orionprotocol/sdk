1a33c419937fd4e5f97869f33d8a89a1839af699
Aleksandr Kraiz <selfsurfer@gmail.com>
2022-11-28 17:50:20 +0400

    New utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,6 +9,9 @@ export { default as denormalizeNumber } from './denormalizeNumber';
 export { default as normalizeNumber } from './normalizeNumber';
 export { default as isNetworkCodeInEnvironment } from './isNetworkCodeInEnvironment';
 export { default as parseExchangeTradeTransaction } from './parseExchangeTradeTransaction';
+export { default as toUpperCase } from './toUpperCase';
+export { default as toLowerCase } from './toLowerCase';
+export { default as isUppercasedNetworkCode } from './isUppercasedNetworkCode';
 
 // export { default as HttpError } from './httpError';
 
