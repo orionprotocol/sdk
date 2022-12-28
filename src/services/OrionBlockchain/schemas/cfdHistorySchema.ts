5cfa8e46ea402bbee5aa25f8e147b58826aee919
Mikhail Gladchenko <m@gladchenko.pw>
2022-12-28 13:06:54 +0000

    cfdHistorySchema was updated

--- src/services/OrionBlockchain/schemas/cfdHistorySchema.ts
+++ src/services/OrionBlockchain/schemas/cfdHistorySchema.ts
@@ -1,15 +1,27 @@
 import { z } from 'zod';
 
-const cfdHistorySchema = z.array(z.object({
+const cfdHistoryItem = z.object({
+  _id: z.string(),
+  __v: z.number(),
   address: z.string(),
   instrument: z.string(),
+  instrumentAddress: z.string(),
   balance: z.string(),
   position: z.string(),
-  reason: z.string(),
+  reason: z.enum(['WITHDRAW', 'DEPOSIT']),
   positionPrice: z.string(),
   fundingRate: z.string(),
   transactionHash: z.string(),
   blockNumber: z.number(),
-}));
+  createdAt: z.number(),
+});
+
+const cfdHistorySchema = z.object({
+  success: z.boolean(),
+  count: z.number(),
+  total: z.number(),
+  pagination: z.object({}),
+  data: z.array(cfdHistoryItem),
+});
 
 export default cfdHistorySchema;
