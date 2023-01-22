52d78664301af0c8736e5f721a866664b35bbd82
kuduzow <admin@intocode.ru>
2023-01-22 17:51:31 +0300

    analytics schema updated

--- src/services/ReferralSystem/schemas/distinctAnalyticsSchema.ts
+++ src/services/ReferralSystem/schemas/distinctAnalyticsSchema.ts
@@ -2,16 +2,18 @@ import { z } from 'zod';
 
 const distinctAnalyticsSchema = z.object({
   referer: z.string(),
-
-  refs_info: z.array(
-    z.object({
-      referral_address: z.string(),
-      referral_earned_fees: z.number(),
-      referer_earned_fees: z.number(),
-      relative_ref_level: z.number(),
-      timestamp: z.number(),
-    }),
-  ),
+  refs_info: z.object({
+    latest_block: z.number(),
+    latest_timestamp: z.number(),
+    referer_earned_fees: z.number(),
+    referral_address: z.string(),
+    referral_earned_fees: z.number(),
+    relative_ref_level: z.number(),
+    reward_record_hash: z.string(),
+    timestamp: z.number(),
+  }),
+  total_earned: z.number(),
+  total_sent_to_governance: z.number(),
 });
 
 export default distinctAnalyticsSchema;
