facf5153dfa3653fbc64167f502e9fdd44b2d1bd
Aleksandr Kraiz <selfsurfer@gmail.com>
2023-03-31 19:22:10 +0400

    Fix imports

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import { simpleFetch, fetchWithValidation } from 'simple-typed-fetch';
 BigNumber.config({ EXPONENTIAL_AT: 1e+9 });
 
 export * as config from './config/index.js';
-export { default as OrionUnit } from './initOrionUnit.js';
+export { default as OrionUnit } from './OrionUnit/index.js';
 export { default as Orion } from './Orion/index.js';
-export { default as initOrionUnit } from './initOrionUnit.js';
 export * as utils from './utils/index.js';
 export * as services from './services/index.js';
 export * as crypt from './crypt/index.js';
--- src/initOrionUnit.ts
+++
@@ -1,8 +0,0 @@
-import OrionUnit from './OrionUnit/index.js';
-
-// backward compatibility
-export default function initOrionUnit(...params: ConstructorParameters<typeof OrionUnit>) {
-  return new OrionUnit(
-    ...params,
-  );
-}
