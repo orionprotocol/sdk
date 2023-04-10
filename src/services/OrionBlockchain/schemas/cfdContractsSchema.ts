6edfef33a94ec0932b57bc405e5e763df55c5561
Mikhail Gladchenko <m@gladchenko.pw>
2023-04-10 08:53:24 +0100

    Updated CFD contracts schema Version 0.18.9

--- src/services/OrionBlockchain/schemas/cfdContractsSchema.ts
+++ src/services/OrionBlockchain/schemas/cfdContractsSchema.ts
@@ -14,6 +14,7 @@ const cfdContractsSchema = z.array(z.object({
   priceIndex: z.number(),
   feePercent: z.number(),
   withdrawMarginLevel: z.number(),
+  delegateContractAddress: z.string(),
 }));
 
 export default cfdContractsSchema;
