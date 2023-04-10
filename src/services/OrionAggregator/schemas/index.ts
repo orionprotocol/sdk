3dbe1b59def5ef4a879466737c22c016e311fc9e
Aleksandr Kraiz <selfsurfer@gmail.com>
2023-04-10 14:39:30 +0400

    Added schemas exports

--- src/services/OrionAggregator/schemas/index.ts
+++ src/services/OrionAggregator/schemas/index.ts
@@ -6,4 +6,6 @@ export { default as pairConfigSchema } from './pairConfigSchema.js';
 export { default as placeAtomicSwapSchema } from './placeAtomicSwapSchema.js';
 export { default as redeemOrderSchema } from './redeemOrderSchema.js';
 export { default as swapInfoSchema } from './swapInfoSchema.js';
+export { default as orderSchema } from './orderSchema.js';
+export * from './aggregatedOrderbookSchema.js'
 export { default as errorSchema } from './errorSchema.js';
