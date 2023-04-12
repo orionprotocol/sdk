10166941a1d63586680adec4c457bb655636c060
Dmitry Leleko <dmitry.leleko@orionprotocol.io>
2023-04-12 16:59:05 +0300

    OP-3702 Update pools schemas

--- src/services/OrionBlockchain/schemas/governancePoolSchema.ts
+++ src/services/OrionBlockchain/schemas/governancePoolSchema.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod';
 
 const governancePoolSchema = z.object({
-  base_apr: z.number(),
-  max_apr: z.number(),
+  base_apr: z.union([z.null(), z.number()]),
+  max_apr: z.union([z.null(), z.number()]),
   tvl: z.string(),
   lp_supply: z.string(),
   lp_staked: z.string(),
   lp_staked_with_boost: z.string(),
   lp_price_in_usd: z.string(),
-  reward_per_period: z.number(),
+  reward_per_period: z.string(),
   lock_time_for_max_multiplier: z.string(),
   lock_max_multiplier: z.string(),
   veorn_max_multiplier: z.string(),
