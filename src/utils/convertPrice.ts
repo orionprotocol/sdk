230c399bdc559c5106ffa22bebf3df1afc4af244
Aleksandr Kraiz <selfsurfer@gmail.com>
2023-08-03 17:08:55 +0400

    Fix: convertPrice

--- src/utils/convertPrice.ts
+++ src/utils/convertPrice.ts
@@ -6,11 +6,11 @@ export default function convertPrice(
   assetOutAddress: string,
   prices: Partial<Record<string, string>> // quoted in quoteAsset. [address]: priceQuotedInQuoteAsset
 ) {
-  const assetInPrice = prices[assetInAddress];
-  if (assetInPrice === undefined) throw Error('assetInPrice is undefined');
+  const assetInPrice = prices[assetInAddress.toLowerCase()];
+  if (assetInPrice === undefined) throw Error(`Price conversion: AssetIn (${assetInAddress}) price is undefined`);
 
-  const assetOutPrice = prices[assetOutAddress];
-  if (assetOutPrice === undefined) throw Error('assetOutPrice is undefined');
+  const assetOutPrice = prices[assetOutAddress.toLowerCase()];
+  if (assetOutPrice === undefined) throw Error(`Price conversion: AssetOut (${assetOutAddress}) price is undefined`);
 
   const assetInPriceBN = new BigNumber(assetInPrice);
   const assetOutPriceBN = new BigNumber(assetOutPrice);
