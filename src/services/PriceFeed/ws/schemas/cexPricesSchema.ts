4a20b90db0afa158ff687c2a1af35f6832468d4e
lambdagit <lambdafxf@gmail.com>
2023-09-05 09:56:45 +0300

    cexPrices in PF

---
+++ src/services/PriceFeed/ws/schemas/cexPricesSchema.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+const cexPriceTickerInfoSchema = z.tuple([
+  z.string(), // pair name
+  z.string(), // lastPrice
+]).transform(([pairName, lastPrice]) => ({
+  pairName:pairName.toUpperCase(),
+  lastPrice,
+}));
+
+type CEXPriceTickerInfo = z.infer<typeof cexPriceTickerInfoSchema>
+
+const cexPricesSchema = z.unknown().array()
+.transform((tickers) => {
+  const data = [...tickers];
+  data.shift();
+  const parsedData = cexPriceTickerInfoSchema.array().parse(data);
+  return parsedData.reduce<
+    Partial<
+      Record<
+        string,
+        CEXPriceTickerInfo
+      >
+    >
+  >((prev, pairData) => ({
+    ...prev,
+    [pairData.pairName]: pairData,
+  }), {});
+});
+
+export default cexPricesSchema;
