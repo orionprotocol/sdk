6cd0995d268310055f06891c5f3ea4f15f30940f
Kirill Litvinov <kigastu@gmail.com>
2023-10-02 15:02:18 +0300

    fix address

--- src/Unit/Exchange/generateSwapCalldata.ts
+++ src/Unit/Exchange/generateSwapCalldata.ts
@@ -46,9 +46,10 @@ export default async function generateSwapCalldata({
   const curveRegistryAddress = safeGet(unit.contracts, 'curveRegistry')
   const { assetToAddress, swapExecutorContractAddress, exchangeContractAddress } = await simpleFetch(unit.blockchainService.getInfo)();
   let path = SafeArray.from(path_).map((swapInfo) => {
-    swapInfo.assetIn = ethers.utils.isAddress(swapInfo.assetIn.toLowerCase()) ? swapInfo.assetIn.toLowerCase() : safeGet(assetToAddress, swapInfo.assetIn);
-    swapInfo.assetOut = ethers.utils.isAddress(swapInfo.assetOut.toLowerCase()) ? swapInfo.assetOut.toLowerCase() : safeGet(assetToAddress, swapInfo.assetOut);
-    console.log('test_', swapInfo.assetIn, swapInfo.assetOut);
+    console.log('test_1', swapInfo.assetIn, swapInfo.assetOut, assetToAddress);
+    swapInfo.assetIn = assetToAddress[swapInfo.assetIn] ?? swapInfo.assetIn.toLowerCase();
+    swapInfo.assetOut = assetToAddress[swapInfo.assetOut] ?? swapInfo.assetOut.toLowerCase();
+    console.log('test_2', swapInfo.assetIn, swapInfo.assetOut);
     return swapInfo;
   })
   const factory = path.first().factory
