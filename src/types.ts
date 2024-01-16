669a6849e75e85116202e45b575837aa921be3fc
Mikhail Gladchenko <m@gladchenko.pw>
2024-01-16 08:43:48 +0000

    bug: fixed type bug

--- src/types.ts
+++ src/types.ts
@@ -48,6 +48,7 @@ export type Order = {
   nonce: number // uint64
   expiration: number // uint64
   buySide: 0 | 1 // uint8, 1=buy, 0=sell
+  isPersonalSign: boolean // bool
 }
 
 export type SignedOrder = {
@@ -59,6 +60,7 @@ export type SignedOrder = {
 export type CancelOrderRequest = {
   id: number | string
   senderAddress: string
+  isPersonalSign: boolean
 }
 
 export type SignedCancelOrderRequest = {
@@ -199,13 +201,6 @@ export type SwapInfoBase = {
   } | undefined
   alternatives: SwapInfoAlternative[]
   assetsNameMapping?: Partial<Record<string, string>> | undefined
-  usdInfo: {
-    availableAmountIn: number | undefined
-    availableAmountOut: number | undefined
-    marketAmountOut: number | undefined
-    marketAmountIn: number | undefined
-    difference: string | undefined
-  } | undefined
 }
 
 export type SwapInfoByAmountIn = SwapInfoBase & {
@@ -261,12 +256,6 @@ export type VerboseUnitConfig = {
       // http://localhost:3003/,
       // http://10.23.5.11:3003/,
       // https://price-feed:3003/
-    }
-    indexer: {
-      api: string
-      // For example:
-      // http://localhost:3004/,
-      // http://
     }
   }
   basicAuth?: BasicAuthCredentials
