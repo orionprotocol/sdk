704bcbca1b5eef912601b76c24b7effae115a66c
Mikhail Gladchenko <m@gladchenko.pw>
2024-04-25 12:15:53 +0100

    feat: tickers schema was updated

--- src/services/Frontage/schemas/tickers-schema.ts
+++ src/services/Frontage/schemas/tickers-schema.ts
@@ -1,11 +1,12 @@
 import { z } from 'zod';
+import { SupportedChainId } from '../../../types';
 
 export const tickerSchema = z.object({
   pair: z.string(),
   volume24: z.number(),
   change24: z.number(),
   lastPrice: z.number(),
-  networks: z.array(z.number()),
+  networks: z.array(z.nativeEnum(SupportedChainId)),
 });
 
 export const tickersSchema = z.array(tickerSchema);
