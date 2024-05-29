af8eb67959e8ced0dcc9007cbe1c5b9f13dee385
TheJuze <dimanikus0106@gmail.com>
2024-05-29 15:05:27 +0300

    add new order status: "TRANSFER"

--- src/constants/orderStatuses.ts
+++ src/constants/orderStatuses.ts
@@ -3,6 +3,7 @@ import subOrderStatuses from './subOrderStatuses.js';
 const orderStatuses = [
   ...subOrderStatuses,
   'ROUTING', // order got sub orders, but not all of them have status ACCEPTED
+  'TRANSFER' // TX_PENDING > TRANSFER > SETTLED
 ] as const;
 
 export default orderStatuses;
