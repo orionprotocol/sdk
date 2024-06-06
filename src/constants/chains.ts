82ff0f7a9c8b9198fd9d387ffc539b05246d2949
TheJuze <dimanikus0106@gmail.com>
2024-06-06 14:58:40 +0300

    actualize testnet chains

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -2,13 +2,7 @@ import { SupportedChainId } from '../types.js';
 
 export const developmentChains = [
   SupportedChainId.BSC_TESTNET,
-  SupportedChainId.ROPSTEN,
-  SupportedChainId.GOERLI,
   SupportedChainId.SEPOLIA,
-  SupportedChainId.ARBITRUM_GOERLI,
-  SupportedChainId.FANTOM_TESTNET,
-  SupportedChainId.POLYGON_TESTNET,
-  SupportedChainId.OKC_TESTNET,
 ];
 export const productionChains = [
   SupportedChainId.MAINNET,
