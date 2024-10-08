3f96abeac15d44b2cc05190bffd2dd61917fce3c
Olga Kanishcheva <okanishcheva@uniqsoft.ae>
2024-10-08 15:54:48 +0400

    Update package.json

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,6 +2,5 @@ export * as aggregator from './Aggregator/index.js';
 export * as blockchainService from './BlockchainService/index.js';
 export * as priceFeed from './PriceFeed/index.js';
 export * as referralSystem from './ReferralSystem/index.js';
-export * as frontage from './Frontage';
 export * as indexer from './Indexer/index.js';
 export * as frontage from './Frontage/index.js';
